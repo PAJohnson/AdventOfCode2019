a900eba6f1afef20fc27616067ffe70b436abfd0
pjohnson <pjohnso1@unca.edu>
2019-12-02 19:23:18 -0500

    Advent of code 2019! Patrick Johnson

---
+++ Day 1/dayOne.py
@@ -0,0 +1,46 @@
+#advent of code day 1
+#read csv into array of numbers
+#process numbers into required fuel (floor(Mass / 3) - 2)
+#sum required fuel
+import csv
+import math
+
+def calcFuel(mass):
+	return math.floor(mass/3)-2
+	
+#part two!
+#calculate the fuel required to carry the fuel (recursion)
+def fuelRequired(mass):
+	Fuel = calcFuel(mass)
+	reqFuel = 0
+	while Fuel > 0:
+		reqFuel = reqFuel + Fuel
+		Fuel = calcFuel(Fuel)
+
+	return reqFuel
+
+mass = []
+
+csvfile = open('input.txt', newline='')
+csvreader = csv.reader(csvfile, delimiter=' ')
+for row in csvreader:
+	mass.append(int(row[0]))
+	
+
+#input file has been read
+
+fuel = []
+for i in range(len(mass)):
+	fuel.append(fuelRequired(mass[i]))
+	
+#fuel required has been computed
+
+sum = 0
+
+for i in range(len(fuel)):
+	sum = sum + fuel[i]
+	
+print(sum)
+
+
+print(fuelRequired(1969))
---
+++ Day 2/dayTwo.py
@@ -0,0 +1,78 @@
+#advent of code day 2
+#simple assembler! cool!
+
+import csv
+
+#might be handy to have a function that parses a list of numbers
+#and returns an array
+def arrayParse(fileName):
+	data = []
+
+	f = open(fileName)
+	fstring = f.read()
+	data = list(map(int,fstring.split(',')))
+	f.close()
+	return data
+
+def opInterpret(opcodes):
+	#opcodes is a list of instructions
+	#this function runs through each instruction
+	newOps = opcodes.copy()
+	index = 0
+	halt = 0
+	while index < len(newOps) and halt == 0:
+		#interpretation
+		if newOps[index] == 1:
+			#add
+			newOps[newOps[index+3]] = newOps[newOps[index+1]] + newOps[newOps[index+2]]
+			#('adding!')
+		elif newOps[index] == 2:
+			#multiply
+			newOps[newOps[index+3]] = newOps[newOps[index+1]] * newOps[newOps[index+2]]
+			#print('multiplying!')
+		elif newOps[index] == 99:
+			#halt
+			halt = 1
+			#print('halting!')
+		else:
+			#error
+			halt = 1
+			#print('unrecognized opcode')
+		
+		index = index + 4
+		#print(index)
+	
+	return newOps
+	
+def findNV(opcodes,key):
+	noun = 0
+	verb = 0
+	newOps = opcodes.copy()
+	result = 0
+	for noun in range(99):
+		for verb in range(99):
+			newOps = opcodes.copy()
+			newOps[1] = noun
+			newOps[2] = verb
+			result = opInterpret(newOps)
+			if result[0] == key:
+				print('Found!')
+				answer = 100*noun + verb
+			
+	return answer
+		
+	
+
+opcodes = arrayParse('input.txt')
+#do some testing
+#do the replacement as described in problem
+opcodes[1] = 12
+opcodes[2] = 2
+result = opInterpret(opcodes)
+print(result[0])
+
+#part 2!
+#find the inputs for opcodes[1] and opcodes[2] that causes
+#opcodes[0] to equal 19690720
+key = 19690720
+print(findNV(arrayParse('input.txt'),key))
